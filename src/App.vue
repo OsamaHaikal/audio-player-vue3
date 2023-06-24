721d677bae00e2bf3e23b06cb9029e8ad5dd1cbe
OsamaHaikal <ee.osamahaikal@gmail.com>
2023-06-24 16:24:47 +0300

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="bg-black2 h-screen grid place-items-center">
+    <div
+      class="bg-white shadow-lg flex items-center justify-center rounded-lg lg:w-[30%] md:w-[60%] sm:w-[50%] p-10"
+    >
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
